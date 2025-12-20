9907fd78a67aed7060a26e606f3452e989206db8
Aren Redd <anandrammohan9952@gmail.com>
2025-12-20 12:36:05 +0000

    Site updated: 2025-12-20 12:36:04

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1766233055116";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1766234149560";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
