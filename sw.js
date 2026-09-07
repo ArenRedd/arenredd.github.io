e33a671c9011b977b2cf9ed529820ca0210a2343
Aren_Redd <anandrammohan9952@gmail.com>
2026-09-07 19:31:40 +0530

    Site updated: 2026-09-07 19:31:39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1788789109869";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1788789697756";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
